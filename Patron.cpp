1c2e62555420b7aafafeccd99d81075e964f4134
Nathan Sosnovske <sosnovsken@spu.edu>
2014-12-05 00:56:48 -0500

    Implementation of Patron class

---
+++ Patron.cpp
@@ -0,0 +1,53 @@
+#include "Patron.h"
+
+Patron::Patron(std::string idn, std::string n)
+{
+    idNum = idn;
+    name = n;
+}
+
+std::vector<Book*> Patron::getCheckedOutBooks()
+{
+    return checkedOutBooks;
+}
+
+double Patron::getFineAmount()
+{
+    return fineAmount;
+}
+
+std::string Patron::getIdNum()
+{
+    return idNum;
+}
+
+std::string Patron::getName()
+{
+    return name;
+}
+
+void Patron::addBook(Book *b)
+{
+    checkedOutBooks.push_back(b);
+}
+
+void Patron::amendFine(double amount)
+{
+    fineAmount += amount;
+}
+
+void Patron::removeBook(Book *b)
+{
+    for(int i = 0; i < checkedOutBooks.size(); i++)
+    {
+        if(b == checkedOutBooks.at(i))
+        {
+            checkedOutBooks.erase(checkedOutBooks.begin() + i);
+            break;
+        }
+        else
+        {
+            continue;
+        }
+    }
+}
