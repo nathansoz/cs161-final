ee0fc0848439c60f209feac2e60370aed5ab26fd
Nathan Sosnovske <sosnovsken@spu.edu>
2014-12-05 00:48:14 -0500

    Implementation of Book class

---
+++ Book.cpp
@@ -0,0 +1,67 @@
+#include "Book.h"
+
+//Constructor for a book. Sets the read-only properties
+
+Book::Book(std::string idc, std::string t, std::string a)
+{
+    idCode = idc;
+    title = t;
+    author = a;
+}
+
+//All of the getters for properties
+std::string Book::getAuthor()
+{
+    return author;
+}
+
+Patron* Book::getCheckedOutBy()
+{
+    return checkedOutBy;
+}
+
+int Book::getDateCheckedOut()
+{
+    return dateCheckedOut;
+}
+
+std::string Book::getIdCode()
+{
+    return idCode;
+}
+
+Locale Book::getLocation()
+{
+    return location;
+}
+
+Patron* Book::getRequestedBy()
+{
+    return requestedBy;
+}
+
+std::string Book::getTitle()
+{
+    return title;
+}
+
+//Setters for non read-only properties
+void Book::setCheckedOutBy(Patron *p)
+{
+    checkedOutBy = p;
+}
+
+void Book::setDateCheckedOut(int d)
+{
+    dateCheckedOut = d;
+}
+
+void Book::setLocation(Locale lo)
+{
+    location = lo;
+}
+
+void Book::setRequestedBy(Patron *p)
+{
+    requestedBy = p;
+}
